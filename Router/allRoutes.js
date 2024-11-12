21fc85edb44cea90dd0367cfe0849e4e403b9ce4
Soizic44 <fetiveau.soizic1@hotmail.fr>
2024-11-12 02:31:28 +0100

    Création des animaux/habitat

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,9 @@ export const allRoutes = [
     new Route("/habitats", "Habitats", "/pages/habitats.html"),
     new Route("/services", "services", "/pages/services.html"),
     new Route("/contact", "contact", "/pages/contact.html"),
+    new Route("/savane", "savane", "/pages/savane.html"),
+    new Route("/jungle", "jungle", "/pages/jungle.html"),
+    new Route("/marais", "marais", "/pages/marais.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
